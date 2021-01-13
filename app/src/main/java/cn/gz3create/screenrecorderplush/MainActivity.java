22364e243706e296ecec2ab789c2ebf9ce32e1be
“卢声波” <rain19870225@163.com>
2021-01-13 22:00:17 +0800

    Initial commit

---
+++ app/src/main/java/cn/gz3create/screenrecorderplush/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.gz3create.screenrecorderplush;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
